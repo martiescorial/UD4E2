8babf0f6e9dc797c025195664dd3280eb64f0012
Martina <martinaescorial9@gmail.com>
2021-06-15 21:08:10 +0200

    Agregar archivos de proyecto.

---
+++ UD4E2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UD4E2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            String nombre = "Nora";
+            String ciudad = "Estocolmo";
+            Console.WriteLine("Hola " + nombre + " bienvenida a " + ciudad);
+            Console.ReadLine();
+        }
+    }
+}
